d0299945940d7a67786d2d6f45a7dcf17f790023
seanclerkin <sean.clerkin@2408-W7E.moneysupermarket.com>
2014-02-11 11:06:09 +0000

    Changed path of epel key

--- puppet/modules/epel/manifests/init.pp
+++ puppet/modules/epel/manifests/init.pp
@@ -26,7 +26,7 @@ class epel {
     owner => 'root',
     group => 'root',
     mode => '0644',
-    source => "puppet:////vagrant/.vagrant-puppet/files/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6"
+    source => "puppet:////opt/config/puppet/files/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6"
   }
 
   epel::key { "RPM-GPG-KEY-EPEL-6":
